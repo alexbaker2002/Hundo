e1c532d0a6f504e2c6c34cc64e8b9658e9bd18c4
Alex R Baker <52913502+alexbaker2002@users.noreply.github.com>
2021-10-04 16:24:59 -0500

    changed index.html to replace name

--- js/site.js
+++
@@ -1,6 +0,0 @@
-function displayMessage(){
-    let msg = document.getElementById("message").value;
-    (msg.length == "") ? msg = "Enter Something... Anything!!" : null;
-  //  alert(msg);
-    Swal.fire(msg);
-}
