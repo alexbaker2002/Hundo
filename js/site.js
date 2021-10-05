41e7d22b1326842994506042e507b242b26f96d3
Alex R Baker <52913502+alexbaker2002@users.noreply.github.com>
2021-10-05 11:26:50 -0500

    refactor site.js

--- js/site.js
+++ js/site.js
@@ -1,62 +1,42 @@
+// main funtion
 function loopyHundo() {
   // get numbers from UI
   let startingNumber = document.getElementById("startValueID").value;
   let endingNumber = document.getElementById("endValueID").value;
 
-
-
   // validate inputs
   startingNumber = parseInt(startingNumber);
   endingNumber = parseInt(endingNumber)
   if (Number.isInteger(startingNumber) && Number.isInteger(endingNumber)) {
-  startingNumber < 100 ? null : startingNumber == 100;
-  endingNumber > 0 ? null : startingNumber == 0;
-
+    startingNumber < 100 ? null : startingNumber == 100;
+    endingNumber > 0 ? null : startingNumber == 0;
     // get the numbers
-   let numbersArray = generateNumbers(startingNumber, endingNumber);
-  // display numbers in UI
-
+    let numbersArray = generateNumbers(startingNumber, endingNumber);
+    // display numbers in UI
     displayNumbersToPage(numbersArray)
-
   } else {
-
-    alert("Enter Numbers")
-
-  }    
-  
+    alert("Please Enter Numbers Only")
+  }
 }
-
 
 //generate the numbers from start to end
-function generateNumbers(start, end){
-
-// array
-let a = []
-
-// Loop through the numbers
-for (let index = start; index <= end; index++) {
-  a.push(index);
+function generateNumbers(start, end) {
+  let a = []
+  // Loop through the numbers
+  for (let index = start; index <= end; index++) {
+    a.push(index);
+  }
+  //returns the array
+  return a;
 }
 
-//returns the array
-return a;
-}
-
-
-
-
-function displayNumbersToPage(numArray){
-
-let startIndex = numArray[0];
-
-let ptag = document.getElementById("resultsID")
-
-for (let index = 0; index < numArray.length; index++) {
-  
-  let number = numArray[index];
- 
-  ptag.innerHTML += number + " | ";
-
-}
-
+function displayNumbersToPage(numArray) {
+  let startIndex = numArray[0];
+  // assign inner tag to var
+  let ptag = document.getElementById("resultsID")
+  //iterate through array and add to UI
+  for (let index = 0; index < numArray.length; index++) {
+    let number = numArray[index];
+    ptag.innerHTML += number + " | ";
+  }
 }
